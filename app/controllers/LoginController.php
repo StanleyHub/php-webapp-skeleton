11c4bd5071ae9c3df38f3293b979f63cf55f2f99
Xiang Zhang <xzhang@thoughtworks.com>
2015-11-03 21:08:33 +0800

    [ZX] add login page.

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -6,7 +6,7 @@ use Firebase\JWT\JWT;
 
 class LoginController {
     function index($f3) {
-
+        echo Template::instance()->render('login.htm');
     }
 
     function login($f3) {
@@ -15,33 +15,27 @@ class LoginController {
 
         $tokenId    = base64_encode(mcrypt_create_iv(32));
         $issuedAt   = time();
-        $notBefore  = $issuedAt + 10;  //Adding 10 seconds
-        $expire     = $notBefore + 60; // Adding 60 seconds
+        $notBefore  = $issuedAt + 10;
+        $expire     = $notBefore + 60;
         $serverName = $f3->get('servername');
 
         $data = [
-            'iat'  => $issuedAt,         // Issued at: time when the token was generated
-            'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
-            'iss'  => $serverName,       // Issuer
-            'nbf'  => $notBefore,        // Not before
-            'exp'  => $expire,           // Expire
-            'data' => [                  // Data related to the signer user
-                'userId'   => 1,         // userid from the users table
-                'userName' => $username, // User name
+            'iat'  => $issuedAt,
+            'jti'  => $tokenId,
+            'iss'  => $serverName,
+            'nbf'  => $notBefore,
+            'exp'  => $expire,
+            'data' => [
+                'userId'   => 1,
+                'userName' => $username
             ]
         ];
 
         $secretKey = $f3->get('jwt.secretkey');
         $algorithm = $f3->get('jwt.algorithm');
-
-        $jwt = JWT::encode(
-            $data,      //Data to be encoded in the JWT
-            $secretKey, // The signing key
-            $algorithm  // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
-        );
-        $unEncodedArray = ['jwt' => $jwt, 'algorithm' => $algorithm];
+        $jwt = JWT::encode($data, $secretKey, $algorithm);
 
         header('Content-type: application/json');
-        echo json_encode($unEncodedArray);
+        echo json_encode(['jwt' => $jwt]);
     }
 }
